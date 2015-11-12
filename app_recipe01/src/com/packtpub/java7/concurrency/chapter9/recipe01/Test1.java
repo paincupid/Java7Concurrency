a30569fba77ee3c8b9602f57a7f2dd6ffa21608d
paincupid <paincupid@hotmail.com>
2015-11-12 12:24:45 +0800

    a
    a

--- app_recipe01/src/com/packtpub/java7/concurrency/chapter9/recipe01/Test1.java
+++ app_recipe01/src/com/packtpub/java7/concurrency/chapter9/recipe01/Test1.java
@@ -4,9 +4,8 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("123");
-		System.out.println("456");
-		System.out.println("789");
+		System.out.println("a");
+		
 	}
 
 }
