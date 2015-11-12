213efc6b25389fb4854022f094015a01ea2e1be6
李东岳 <wb-lidongyue@alibaba-inc.com>
2015-11-12 12:21:05 +0800

    test3a

--- app_recipe01/src/com/packtpub/java7/concurrency/chapter9/recipe01/Test3.java
+++ app_recipe01/src/com/packtpub/java7/concurrency/chapter9/recipe01/Test3.java
@@ -1,5 +1,8 @@
 package com.packtpub.java7.concurrency.chapter9.recipe01;
 
 public class Test3 {
-
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("a");
+	}
 }
